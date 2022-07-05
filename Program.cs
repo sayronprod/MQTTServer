2b66d8fb6bb1e8cd6ec59db87abd4f70d9e785b8
Sirion <cool.sirion@gmail.com>
2022-07-06 00:15:11 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using MQTTnet.Server.Web;
+using System;
+using System.IO;
+
+namespace MQTTnet.Server
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.ConfigureKestrel(serverOptions =>
+                        {
+                        })
+                        .UseWebRoot(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Web", "wwwroot"))
+                        .UseStartup<Startup>();
+                    })
+                    .UseWindowsService()
+                    .Build()
+                    .Run();
+
+                return 0;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return -1;
+            }
+        }
+    }
+}
